7bbabf33672d72cf951ffd01ba4d51ec7a737f85
Alfian57 <alfiangadingsaputra@gmail.com>
2024-06-02 18:26:44 +0700

    fix: registration feature

--- users/serializers.py
+++ users/serializers.py
@@ -8,23 +8,44 @@ User = get_user_model()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(required=True)
+    phone_number = serializers.IntegerField(required=True)
     email = serializers.EmailField(
+        required=True, validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
         required=True, validators=[UniqueValidator(queryset=User.objects.all())]
     )
     password = serializers.CharField(
         write_only=True, required=True, validators=[validate_password]
     )
+    password_confirmation = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ("name", "email", "username", "phone_number", "password")
+        fields = (
+            "name",
+            "email",
+            "username",
+            "phone_number",
+            "password",
+            "password_confirmation",
+        )
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password_confirmation"]:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
+        return attrs
 
     def create(self, validated_data):
+        validated_data.pop("password_confirmation")
         user = User.objects.create(
             username=validated_data["username"],
             email=validated_data["email"],
-            name=validated_data["name"],
-            phone_number=validated_data["phone_number"],
+            name=validated_data.get("name", ""),
+            phone_number=validated_data.get("phone_number", ""),
         )
         user.set_password(validated_data["password"])
         user.save()
